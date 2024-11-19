eeff17b64b68a59b1a26d66e2a839ffbf363d439
Fernando <feriley8@gmail.com>
2024-11-19 15:44:34 +0100

    Inicio la rama DEVELOP

---
+++ src/test/java/com/example/proyectoServidor/ProyectoServidorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.proyectoServidor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoServidorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
